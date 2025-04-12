8f0ca980f97ce5dadf912d2ab7d0a05d49fb6401
hardikrana2515 <hardik25rana@gmail.com>
2025-04-12 09:01:26 +0530

    commit my page...

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders the quote button', () => {
-//   render(<App />);
-//   const buttonElement = screen.getByRole('button', { name: /new quote/i });
-//   expect(buttonElement).toBeInTheDocument();
-// });
